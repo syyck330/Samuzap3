82a24c755c4e2af4bf9bf5be5fca5992b95d1852
syyck330 <108939893+syyck330@users.noreply.github.com>
2025-04-11 17:27:09 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-const client = require('./whatsapp');
 
-console.log("Chatbot Medeiros Calçados iniciado, aguardando conexão WhatsApp...");
+// Importa o módulo whatsapp, que agora contém o processo de autenticação
+require('./whatsapp');
+
+console.log("Chatbot Medeiros Calçados iniciado, aguardando escolha do método de autenticação...");
